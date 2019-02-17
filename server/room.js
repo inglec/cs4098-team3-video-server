637a7e0d6874ff539dc3349a618f042273d9899e
Eddie Bergman <eddiebergmanhs@gmail.com>
2019-02-17 16:39:03 +0000

    Changed room to Class and  have explicit functions per event

--- server/room.js
+++ server/room.js
@@ -1,65 +1,90 @@
-function Room(config, msroom){
+const logger = require('./logme')
 
-  this._room = msroom;
-  this.peers = new Map();
-  this.id = config.id;
+class Room{
 
-  this.addPeer = (socket, user) => {
+  constructor(config, msroom) {
+    this._room = msroom;
+    this.peers = new Map();
+    this.id = config.id;
+  }
 
+  receiveJoin(request, socket, user, cb) {
+    this._room.receiveRequest(request)
+      .then( (response) => {
+        logger.info("User ", user.peerId, " joined room", this.id);
+        mpeer = this._room.getPeerByName(user.peerId);
+        this._setNewPeerHandles(mpeer, user, socket);
+        this.peers.add(user.peerId, mpeer);
+        cb(null, response);
+      })
+      .catch( (error) => {
+        logger.error("Failed to respond to", request, "\n", error);
+        cb(error)
+      });
+  }
 
+  receiveQueryRoom(request, cb) {
+    this._room.receiveRequest(request)
+      .then( (response) => {
+        logger.info("User ", user.peerId, " queried room", this.id);
+        cb(null, response);
+      })
+      .catch( (error) => {
+        logger.error("Failed to respond to", request, "\n", error);
+        cb(error)
+      });
+  }
 
-    //should hopefully create a peer if non-exists
-    const peer = this._room.getPeerByName(user.peerId);
-    this.peers.add(user.peerId, peer);
-
-    logger.info("Peer added to room:", this.id);
-    logger.info("Peer ", user.peerId);
+  _setNewPeerHandles(peer, socket) {
 
     peer.on('notify', (notification) => {
-      console.log('New notification for mediaPeer received:', notification);
+      logger.info('New notification for mediaPeer received:', notification);
       socket.emit('mediasoup-notification', notification);
     });
 
     peer.on('newtransport', (transport) => {
-      console.log('New mediaPeer transport:', transport.direction);
+      logger.info('New mediaPeer transport:', transport.direction);
       transport.on('close', (originator) => {
-        console.log('Transport closed from originator:', originator);
+        logger.info('Transport closed from originator:', originator);
       });
     });
 
     peer.on('newproducer', (producer) => {
-      console.log('New mediaPeer producer:', producer.kind);
+      logger.info('New mediaPeer producer:', producer.kind);
       producer.on('close', (originator) => {
-        console.log('Producer closed from originator:', originator);
+        logger.info('Producer closed from originator:', originator);
       });
     });
 
     peer.on('newconsumer', (consumer) => {
-      console.log('New mediaPeer consumer:', consumer.kind);
+      logger.info('New mediaPeer consumer:', consumer.kind);
       consumer.on('close', (originator) => {
-        console.log('Consumer closed from originator', originator);
+        logger.info('Consumer closed from originator', originator);
       });
     });
 
     // Also handle already existing Consumers.
     peer.consumers.forEach((consumer) => {
-      console.log('mediaPeer existing consumer:', consumer.kind);
+      logger.info('mediaPeer existing consumer:', consumer.kind);
       consumer.on('close', (originator) => {
-        console.log('Existing consumer closed from originator', originator);
+        logger.info('Existing consumer closed from originator', originator);
       });
     });
 
     socket.on('disconnect', () => {
-      if(peer)
+      logger.info("Peer has closed the connection", user);
+      this.peers.delete(user.peerId);
+      if(peer){
         peer.close()
+      }
     });
   }
 
-  this.getPeer = (user) => {
+  getPeer(user) {
     return peers.get(user.peerId);
   }
 
-  this.onClose = (onCloseCallback) => {
+  onClose(onCloseCallback) {
     this._room.on('close', onCloseCallback);
   }
 
